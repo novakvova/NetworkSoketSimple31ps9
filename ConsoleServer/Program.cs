0e9f4baf7bece8b93af29968790019e71988cb4b
novakvova <novakvova@gmail.com>
2019-05-11 11:41:55 +0300

    Server add data cleint

--- ConsoleServer/Program.cs
+++ ConsoleServer/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,31 @@ namespace ConsoleServer
         {
             Socket s = new Socket(AddressFamily.InterNetwork,
                 SocketType.Stream, ProtocolType.IP);
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            IPEndPoint ep = new IPEndPoint(ip, 1098);
+            Console.Title = "Server " + ep.ToString();
+            s.Bind(ep); //Наш сокет звязаний з даною адресою
+            s.Listen(10);
+            try
+            {
+                while (true)
+                {
+                    Socket ns = s.Accept();
+                    string data = null;
+                    // Мы дождались клиента, пытающегося с нами соединиться
+
+                    byte[] bytes = new byte[1024];
+                    int bytesRec = ns.Receive(bytes);
+
+                    data += Encoding.UTF8.GetString(bytes, 0, bytesRec);
+                    Console.WriteLine("Нам прислали: " + data);
+
+                }
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine("Socket problem: " + ex.Message);
+            }
         }
     }
 }
