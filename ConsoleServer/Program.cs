e571e692e8f18601e351b182f1b4f666724d73db
novakvova <novakvova@gmail.com>
2019-05-11 09:51:56 +0300

    Begin network app

---
+++ ConsoleServer/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket s = new Socket(AddressFamily.InterNetwork,
+                SocketType.Stream, ProtocolType.IP);
+        }
+    }
+}
